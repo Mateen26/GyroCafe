b31749cc818b85d20af31a0c04c58065c8fef1a2
Muhammad Mateen <mateenrajput55@gmail.com>
2025-12-28 00:46:50 +0500

    commeting the link for now

--- src/lib/config.js
+++ src/lib/config.js
@@ -29,7 +29,7 @@ export const siteConfig = {
     { label: "Menu", href: "/menu" },
     { label: "Sauced", href: "/sauced" },
     { label: "Catering", href: "/catering" },
-    { label: "Catering Menu", href: "/catering-menu" },
+    // { label: "Catering Menu", href: "/catering-menu" },
     { label: "Promotions", href: "/promotions", icon: "🔥" },
     { label: "Reviews", href: "/reviews" },
     { label: "Contact Us", href: "/contact" },
